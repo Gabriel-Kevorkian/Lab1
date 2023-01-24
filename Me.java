25b30c10c3f0e0f0f262946939250c996534a1f5
Gabriel Kevorkian <gabrielkevorkian2005@gmail.com>
2023-01-24 19:51:57 +0100

    My name, age ,major

---
+++ Me.java
@@ -0,0 +1,7 @@
+public class Me{
+	public static void main (String[] args ){
+		System.out.println("Gabriel Kevorkian");
+		System.out.println("17");
+		System.out.println("computer engeneering");
+	}
+} 
