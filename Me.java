fb725d33af67caf9c7f75eff8732f20bcfe532a5
Gabriel-Kevorkian <122911025+Gabriel-Kevorkian@users.noreply.github.com>
2023-09-15 22:14:24 +0300

    Me.java

--- Me.java
+++ Me.java
@@ -2,7 +2,7 @@ public class Me{
 	public static void main (String[] args ){
 		System.out.println("Gabriel Kevorkian");
 		System.out.println("17");
-		System.out.println("computer engeneering");
+		System.out.println("computer engineering");
 		System.out.println("YEAR:2022");
 	}
 } 
